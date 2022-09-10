d70f4e978b2cac49572db55cf9d6e06a24cff588
Marowalker <binhnguyen2798@gmail.com>
2022-09-10 13:59:12 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import constants
+from preprocessing import get_sequences, load_pickle
+import pickle
+from models.transE import TransEModel
+import tensorflow as tf
+
+
+def main_knowledge_base():
+    if constants.IS_REBUILD == 1:
+        print('Build data...')
+        train_dict, val_dict = get_sequences(constants.ALL_WORDS, constants.ALL_DEPENDS, constants.ALL_EDGES,
+                                             constants.PICKLE + 'train.pkl', constants.PICKLE + 'dev.pkl')
+        with open(constants.PICKLE + 'train.pkl', 'wb') as f:
+            pickle.dump(train_dict, f)
+
+        with open(constants.PICKLE + 'val.pkl', 'wb') as f:
+            pickle.dump(val_dict, f)
+    else:
+        print('Load data...')
+        with open(constants.PICKLE + 'train.pkl', 'rb') as f:
+            train_dict = pickle.load(f)
+
+        with open(constants.PICKLE + 'val.pkl', 'rb') as f:
+            val_dict = pickle.load(f)
+
+    print("Train shape: ", len(train_dict['head']))
+    print("Validation shape: ", len(val_dict['head']))
+
+    with tf.device('/device:GPU:0'):
+
+        transe = TransEModel(model_path=constants.TRAINED_MODELS, batch_size=64, epochs=constants.EPOCHS,
+                             score=constants.SCORE)
+        transe.build(train_dict, val_dict)
+        # transe.train(early_stopping=True, patience=constants.PATIENCE)
+        all_emb = transe.load(load_file='data/embeddings/transe_cdr_word_' + str(constants.INPUT_W2V_DIM) + '.pkl',
+                              embed_type='word')
+        print(all_emb)
+        print(all_emb.shape)
+
+
+if __name__ == '__main__':
+    main_knowledge_base()
---
+++ preprocessing.py
@@ -0,0 +1,81 @@
+import random
+from data_utils import load_vocab
+import pickle
+from sklearn.utils import shuffle
+
+
+def create_triple(infile, ent_vocab, rel_vocab):
+    file = open(infile)
+    lines = file.readlines()
+    lines = [line.strip() for line in lines]
+    ents = list(ent_vocab.keys())
+
+    heads = []
+    tails = []
+    rels = []
+    heads_neg = []
+    tails_neg = []
+
+    for idx, line in enumerate(lines):
+        if idx != 0:
+            triple = line.split()
+            head, tail, rel = triple[0], triple[1], triple[2]
+            head_id = ent_vocab[head]
+            tail_id = ent_vocab[tail]
+            rel_id = rel_vocab[rel]
+            threshold = random.random()
+            if threshold < 0.5:
+                head_neg = random.choice(ents)
+                head_neg_id = ent_vocab[head_neg]
+                tail_neg_id = tail_id
+            else:
+                tail_neg = random.choice(ents)
+                tail_neg_id = ent_vocab[tail_neg]
+                head_neg_id = head_id
+            heads.append(head_id)
+            tails.append(tail_id)
+            rels.append(rel_id)
+            heads_neg.append(head_neg_id)
+            tails_neg.append(tail_neg_id)
+
+    return heads, tails, rels, heads_neg, tails_neg
+
+
+def get_sequences(ent_file, rel_file, triple_file, outfile_train, outfile_dev):
+    ent_vocab = load_vocab(ent_file)
+    rel_vocab = load_vocab(rel_file)
+
+    head, tail, rel, head_neg, tail_neg = create_triple(triple_file, ent_vocab, rel_vocab)
+
+    head_shuffled, tail_shuffled, rel_shuffled, head_neg_shuffled, tail_neg_shuffled = shuffle(head, tail, rel,
+                                                                                               head_neg, tail_neg)
+
+    ratio = 0.8
+    n_sample = int(len(head) * ratio)
+
+    sequence_dict_train = {
+        'head': head_shuffled[:n_sample],
+        'tail': tail_shuffled[:n_sample],
+        'rel': rel_shuffled[:n_sample],
+        'head_neg': head_neg_shuffled[:n_sample],
+        'tail_neg': tail_neg_shuffled[:n_sample]
+    }
+    sequence_dict_dev = {
+        'head': head_shuffled[n_sample:],
+        'tail': tail_shuffled[n_sample:],
+        'rel': rel_shuffled[n_sample:],
+        'head_neg': head_neg_shuffled[n_sample:],
+        'tail_neg': tail_neg_shuffled[n_sample:]
+    }
+
+    return sequence_dict_train, sequence_dict_dev
+
+
+def load_pickle(train_file, dev_file):
+    with open(train_file, 'rb') as f:
+        sequence_dict_train = pickle.load(f)
+
+    with open(dev_file, 'rb') as f:
+        sequence_dict_dev = pickle.load(f)
+
+    return sequence_dict_train, sequence_dict_dev
